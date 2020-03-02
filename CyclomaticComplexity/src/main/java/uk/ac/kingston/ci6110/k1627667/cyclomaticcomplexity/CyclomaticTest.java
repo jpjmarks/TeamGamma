4274c5a8b3d555e5d336da759ea22b61f26f3479
James <jpjmarks@gmail.com>
2020-03-02 13:53:49 +0000

    added more comments to current code added extra lines to test file for string and comment tests

--- CyclomaticComplexity/src/main/java/uk/ac/kingston/ci6110/k1627667/cyclomaticcomplexity/CyclomaticTest.java
+++ CyclomaticComplexity/src/main/java/uk/ac/kingston/ci6110/k1627667/cyclomaticcomplexity/CyclomaticTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @author james
  */
 public class CyclomaticTest {
-
+ 
     // JM
     public ArrayList<String> cycloTests(ArrayList<String> file) {
         ArrayList<ArrayList<String>> methods = (ArrayList<ArrayList<String>>) seaparateMethods(file).clone(); 
@@ -20,9 +20,11 @@ public class CyclomaticTest {
         ArrayList<String> scores = new ArrayList<String>();
         for (int i = 0; i < methods.size(); i++) // iterates through methods array list ignoring the first element which is the name of the method
         {
-            int runningTotal = 1;
+            int runningTotal = 1; //running total opf the complexity, starting at 1 as all methods if runnable will have a complexity of 0 even with no code
             //System.out.println(methods.get(i)); //debug
-            methods.set(i, commentTest(methods.get(i))) ;
+            methods.set(i, commentTest(methods.get(i))) ; //comment and string tests to remove any  mentions of trigger words
+           //each of the following lines runs each individual test on each arraly list returning the score for each test
+           //possibly display the scores for individual tests later
             runningTotal = runningTotal + ifTest(methods.get(i));
             runningTotal = runningTotal + caseTest(methods.get(i));
             runningTotal = runningTotal + defaultTest(methods.get(i));
@@ -38,58 +40,57 @@ public class CyclomaticTest {
             runningTotal = runningTotal + andTest(methods.get(i));
             runningTotal = runningTotal + ternaryTest(methods.get(i));
             runningTotal = runningTotal + returnTest(methods.get(i));
-            scores.add(methods.get(i).get(0) + " has a cyclomatic Score of : " + Integer.toString(runningTotal));
+            scores.add(methods.get(i).get(0) + " has a cyclomatic Score of : " + Integer.toString(runningTotal)); // adds total score to an array to dispaly as he result.
             scores.add("");
         }
 
-        return scores;
+        return scores; //returns the array of scores
     }
 
-    public ArrayList<ArrayList<String>> seaparateMethods(ArrayList<String> file) {        
-        ArrayList<ArrayList<String>> outArray = new ArrayList<ArrayList<String>>(); // initiates new arraylist of arraylists
+    public ArrayList<ArrayList<String>> seaparateMethods(ArrayList<String> file)  //initiates separate methods methof with an input of arraylist of type string, returning an array list of array lists
+    {        
+        ArrayList<ArrayList<String>> outArray = new ArrayList<ArrayList<String>>(); // initiates new 2d arraylist
         ArrayList<String> methodNames = new ArrayList<String>(); // initiates arraylist used to store names od methods
         String tempString = ""; // creates empty string for temporarily soring each file arraylist element in
-        int numOfMethods = 0; // keeps running total of number of methods, used to acess array indexes
-        int methodLines = 0;
         ArrayList<Integer> methodStartLinesList = new ArrayList<Integer>();
         ArrayList<Integer> methodEndLinesList = new ArrayList<Integer>();
         for (int i = 0; i < file.size(); i++) // for loop to iterate through each array element
         {
             tempString = file.get(i); // adds array element at index i to temp string
-            if (tempString.contains("main") && tempString.contains("("))
+            if (tempString.contains("main") && tempString.contains("(")) //checks if method is main method
             { 
-                if (methodLines > 1)
+                if (methodNames.size()> 0) //checks if this is the first method in the file
                 {
-                    methodEndLinesList.add(i-1);
+                    methodEndLinesList.add(i-1); //adds end line of previous method to array
                 }
-                methodStartLinesList.add(i);
-                methodLines =1;
-                numOfMethods++;
-                methodNames.add("main");
-                outArray.add(new ArrayList<String>());
-                outArray.get(numOfMethods - 1).add("Line: " + i + " Method: " + methodNames.get(numOfMethods - 1));
+                methodStartLinesList.add(i); //adds start line of current method
+                 //increases the number of methods by 1
+                methodNames.add("main"); //adds "main" to the array of method names
+                outArray.add(new ArrayList<String>()); //adds a new array list to the output 2d array
+                outArray.get(methodNames.size()  - 1).add("Line: " + i + " Method: " + methodNames.get(methodNames.size()  - 1)); //sets the name of the method as the first element of each arraylist in turn
             }
-            else if ((tempString.contains("public") || tempString.contains("protected") || tempString.contains("private"))
+
+            //!FIX NEEDED! NEED TO CHECK FOR PRESNCE OF '{' ON CURRENT OR NEXT LINE 
+
+            else if ((tempString.contains("public") || tempString.contains("protected") || tempString.contains("private")) //checks if the current line contains any modifiers that indicate methods
                     && tempString.contains("(")) // checks each array elemeth for the strings "public" and "(" as these determine
             {
-                if (methodLines > 1)
+                if (methodNames.size()> 0) //checks if this is the first method in the file
                 {
-                    methodEndLinesList.add(i-1);
+                    methodEndLinesList.add(i-1); // adds end line of previous method to array
                 }
-                String[] arrOfStr = tempString.split(" ", 0);
-                if (arrOfStr[2].contains("(")) {
-                    methodStartLinesList.add(i);
-                    methodLines =1;
-                    numOfMethods++;
+                String[] arrOfStr = tempString.split(" ", 0); // removes spaces and splits words
+                if (arrOfStr[2].contains("(")) { //checks if the 3rd element contains ( as most methods will have this or the will not run
+                    methodStartLinesList.add(i); // makes note of the line this method starts on
+                    // 
                     String[] arrOfStr2 = arrOfStr[2].split("\\(", 0);
                     methodNames.add(arrOfStr2[0]);
                     outArray.add(new ArrayList<String>());
-                    outArray.get(numOfMethods - 1).add(methodNames.get(numOfMethods - 1));
+                    outArray.get(methodNames.size()  - 1).add(methodNames.get(methodNames.size() - 1));
                 }
             }
             if (methodNames.size() > 0) {
-               methodLines++;
-               outArray.get(numOfMethods - 1).add(tempString);
+               outArray.get(methodNames.size() - 1).add(tempString);
             }
             if (i == file.size()-1)
             {
@@ -100,8 +101,16 @@ public class CyclomaticTest {
         // System.out.println("num of start lines"+methodStartLinesList.size()); //debug
         // System.out.println("num of end lines"+methodEndLinesList.size()); //debug
         // System.out.println("num of methods"+methodNames.size()); //debug
+        // for (int i = 0; i< methodStartLinesList.size();i++)
+        // {
+        //     System.out.println("start lines "+methodStartLinesList.get(i)); //debug
 
-
+        // }
+        // for (int i = 0; i< methodEndLinesList.size();i++)
+        // {
+        //     System.out.println("End lines "+methodEndLinesList.get(i)); //debug
+        // }
+ 
         for (int i = 0; i < outArray.size(); i++) {
           outArray.get(i).set(0, "Lines: "+methodStartLinesList.get(i)+" - "+ methodEndLinesList.get(i) + " Method: " + methodNames.get(i) );
         }
@@ -206,6 +215,7 @@ public class CyclomaticTest {
     }
 
     // JK
+    //added cose to testcode2 in method getGameState to help test this
     public ArrayList<String> commentTest(ArrayList<String> method) {
 
         return method;
