f5626a5453b5292fd5466fd77c36420141bfab77
James <jpjmarks@gmail.com>
2020-03-13 18:25:19 +0000

    moved files to correct locations changed pckage info

--- CyclomaticComplexity/src/main/java/uk/ac/kingston/ci6110/k1627667/cyclomaticcomplexity/GammaTool.java
+++ CyclomaticComplexity/src/main/java/uk/ac/kingston/ci6110/k1627667/cyclomaticcomplexity/GammaTool.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gammatool;
+package uk.ac.kingston.ci6110.k1627667.cyclomaticcomplexity;
 
 import java.io.BufferedReader;
 import java.io.File;
